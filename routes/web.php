a5a66a2ebff33c1bbbd13c5ef148a315f0af1142
Dylan <cerobreath@gmail.com>
2024-12-23 15:45:15 +0200

    Failed implementation of the cashier page

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,19 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::get('/management', function (){
     return view('management.index');
 });
+
 Route::resource('/management/category','\App\Http\Controllers\Management\CategoryController');
 Route::resource('/management/menu','\App\Http\Controllers\Management\MenuController');
 Route::resource('/management/table','\App\Http\Controllers\Management\TableController');
+
+Route::get('/cashier', '\App\Http\Controllers\Cashier\CashierController@index');
+Route::get('/cashier/getMenuByCategory/{category_id}', '\App\Http\Controllers\Cashier\CashierController@getMenuByCategory');
+Route::get('/cashier/getTable', '\App\Http\Controllers\Cashier\CashierController@getTables');
+Route::get('/cashier/getSaleDetailsByTable/{table_id}', '\App\Http\Controllers\Cashier\CashierController@getSaleDetailsByTable');
+
+Route::post('/cashier/orderFood', '\App\Http\Controllers\Cashier\CashierController@orderFood');
+Route::post('/cashier/deleteSaleDetail', '\App\Http\Controllers\Cashier\CashierController@deleteSaleDetail');
+
+Route::post('/cashier/confirmOrderStatus', '\App\Http\Controllers\Cashier\CashierController@confirmOrderStatus');
+Route::post('/cashier/savePayment', '\App\Http\Controllers\Cashier\CashierController@savePayment');
+Route::get('/cashier/showReceipt/{saleID}', '\App\Http\Controllers\Cashier\CashierController@showReceipt');
